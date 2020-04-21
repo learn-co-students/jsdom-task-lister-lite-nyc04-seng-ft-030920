8201dfc509aff8fb216d9799cb546cb40cf0a1a7
StephanieZou1 <stephanie.zou3@gmail.com>
2020-04-21 16:34:26 -0400

    deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+// submit
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    let theFormWeAreWorkingWith = evt.target;
+    let input = theFormWeAreWorkingWith["new-task-description"]
+    let str = input.value
+    console.log(`${str}`)
+    createTask(str)
+  })
+  function createTask(task) {
+    let todos = document.querySelector("ul#tasks")
+    let blankLi = document.createElement("li")
+    blankLi.innerText = task
+    let button = document.createElement("button")
+    button.innerText = "x"
+    blankLi.append(button)
+    todos.append(blankLi)
+
+    button.addEventListener("click", () => {
+      blankLi.remove()
+    })
+  }
 });
+
+//<button data-description="groceries">X</button>
