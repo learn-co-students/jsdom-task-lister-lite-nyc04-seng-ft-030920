b62154db96ca316776f3f29eb45eeec5894aaa7a
isabel k. lee <hello@isabelklee.co>
2020-04-21 21:28:10 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,34 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   const taskForm = document.querySelector("#create-task-form")
+  // identifies the entire form
+
   const taskDescription = document.querySelector("#new-task-description")
+  // identifies just the input field
+
   const taskList = document.getElementById("tasks")
+  // identifies the list of tasks
 
   // another way of pulling this list =>
-  // let list = document.querySelector("div#list ul")
+  // const taskList = document.querySelector("div#list ul")
 
   taskForm.addEventListener("submit", (event) => {
     event.preventDefault()
+    // prevents the page from reloading when the user hits "submit"
+
     const task = document.createElement("li")
+    // create a new "li" element for the user's new task
+
     task.innerText = taskDescription.value
+    // insert the user's input in the field as the new task's value
 
     taskList.append(task)
+    // associate the child "li" element to the parent "ul"
 
     event.target.reset()
+    // clears the form input field
     }
   )
 });
-
-  // submitButton.addEventListener("submit", (event) => {
-  //   event.preventDefault()
-
-  //   let form = event.target
-  //   let newInput = form["new-task-description"]
-  //   let string = newInput.value
-
-  //   let task = document.createElement("li")
-  //   task.innerText = string
-  //   list.append(task)
-
-  //   // reset clears the inputs of a form
-  //   event.target.reset()
-  // })
